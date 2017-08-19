1a21528f61d4d633450e63aff809c7706b130122
Matt Gibson <matt@mpgibson.com>
2017-08-19 16:25:08 +0100

    proof of concept

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -4,7 +4,8 @@ class BusinessesController < ApplicationController
   # GET /businesses
   # GET /businesses.json
   def index
-
+    @client = GooglePlaces::Client.new("AIzaSyDlFqxHqgFzsbL-Pxh7TAxlwiPfyLzjMsY")
+    @places = @client.spots(53.5354934, -1.1757078, :radius => 1000) #1000m
   end
 
   # GET /businesses/1
