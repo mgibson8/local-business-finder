443d160b8a2b2b0927c2d4876d3fb309cfacbf93
Matt Gibson <matt@mpgibson.com>
2017-08-21 21:26:31 +0100

    list

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,19 +1,23 @@
 class BusinessesController < ApplicationController
+  before_action :set_google_client
   # GET /businesses
   # GET /businesses.json
   def index
-    @client = GooglePlaces::Client.new("AIzaSyDlFqxHqgFzsbL-Pxh7TAxlwiPfyLzjMsY")
-    @places = @client.spots(53.5354934, -1.1757078, :radius => 1000) #1000m
   end
 
   def list
-    binding.pry
+    @postcode = params[:postcode]
+    location  = Geokit::Geocoders::GoogleGeocoder.geocode(@postcode)
+    places    = @client.spots(53.5354934, -1.1757078, :radius => 1000) #1000m
   end
 
   private
     # Use callbacks to share common setup or constraints between actions
+    def set_google_client
+      @client = GooglePlaces::Client.new("AIzaSyDlFqxHqgFzsbL-Pxh7TAxlwiPfyLzjMsY")
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def business_params
-      params.require(:business).permit(:name)
+      params.require(:business).permit(:postcode)
     end
 end
