5311aae8752e78ef9f6154d4f22058702a49c20f
Matt Gibson <matt@mpgibson.com>
2017-08-21 21:15:34 +0100

    loads of changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'businesses#index'
   resources :businesses
+  get 'businesses/list' , to: 'businesses#list'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
