8b765b514dd383f97487c59252035c2c8231ed0b
Matt Gibson <matt@mpgibson.com>
2017-08-22 23:48:35 +0100

    hide checkboxes for now

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,5 +1,6 @@
 class BusinessesController < ApplicationController
   before_action :set_google_client
+  before_action :set_types, only: [:index]
   # GET /businesses
   # GET /businesses.json
   def index
@@ -10,8 +11,8 @@ class BusinessesController < ApplicationController
     @radius   = params[:radius].to_i
     redirect_to root_url and return unless @postcode.present?
     @radius_in_metres = @radius * 1.60934 * 1000
-    @location   = Geokit::Geocoders::GoogleGeocoder.geocode(@postcode)
-    @places    = @client.spots(@location.lat, @location.lng, radius: @radius*1.60934*1000, detail: true)
+    @location = Geokit::Geocoders::GoogleGeocoder.geocode(@postcode)
+    @places   = @client.spots(@location.lat, @location.lng, radius: @radius*1.60934*1000, detail: true)
 
 
     respond_to do |format|
@@ -44,6 +45,9 @@ class BusinessesController < ApplicationController
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def business_params
-      params.require(:business).permit(:postcode, :radius, :lat, :lng, :rad)
+      params.require(:business).permit!
+    end
+    def set_types
+      @types = ["accounting","airport","amusement_park","aquarium","art_gallery","atm","bakery","bank","bar","beauty_salon","bicycle_store","book_store","bowling_alley","bus_station","cafe","campground","car_dealer","car_rental","car_repair","car_wash","casino","cemetery","church","city_hall","clothing_store","convenience_store","courthouse","dentist","department_store","doctor","electrician","electronics_store","embassy","establishment","finance","fire_station","florist","food","funeral_home","furniture_store","gas_station","general_contractor","grocery_or_supermarket","gym","hair_care","hardware_store","health","hindu_temple","home_goods_store","hospital","insurance_agency","jewelry_store","laundry","lawyer","library","liquor_store","local_government_office","locksmith","lodging","meal_delivery","meal_takeaway","mosque","movie_rental","movie_theater","moving_company","museum","night_club","painter","park","parking","pet_store","pharmacy","physiotherapist","place_of_worship","plumber","police","post_office","real_estate_agency","restaurant","roofing_contractor","rv_park","school","shoe_store","shopping_mall","spa","stadium","storage","store","subway_station","synagogue","taxi_stand","train_station","transit_station","travel_agency","university","veterinary_care","zoo"]
     end
 end
