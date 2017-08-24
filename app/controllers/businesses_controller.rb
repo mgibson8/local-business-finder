f86d8c5324b35bfb36a16528743596f104e08bd5
Matt Gibson <matt@mpgibson.com>
2017-08-24 15:49:08 +0100

    hacked in download csv

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -11,13 +11,13 @@ class BusinessesController < ApplicationController
     @radius   = params[:radius].to_i
     redirect_to root_url and return unless @postcode.present?
     @radius_in_metres = @radius * 1.60934 * 1000
-    binding.pry
     @location = Geokit::Geocoders::GoogleGeocoder.geocode(@postcode)
-    @places   = @client.spots(@location.lat, @location.lng, radius: @radius*1.60934*1000, detail: true)
+    @places   = @client.spots(@location.lat, @location.lng, radius: @radius_in_metres, detail: true)
 
 
     respond_to do |format|
       format.js
+      format.csv { send_data gen_csv(@places), filename: "companies-#{Date.today}.csv", type: "text/csv"}
     end
   end
 
@@ -27,15 +27,16 @@ class BusinessesController < ApplicationController
     radius = params[:rad]
     places = @client.spots(lat, lng, radius: radius, detail: true)
 
-    businesses = Array.new
-    businesses << ["Name", "Address", "Phone", "Type"]
+    @businesses = Array.new
+    #@businesses << ["Name", "Address", "Phone", "Type"]
     places.each do |place|
-      businesses << [place.name, place.formatted_address, place.formatted_phone_number, place.types.map(&:humanize).join('/')]
+      @businesses << [place.name, place.formatted_address, place.formatted_phone_number, place.types.map(&:humanize).join('/')]
     end
 
     respond_to do |format|
-      format.html
-      format.csv { send_data businesses.to_csv, filename: "companies-#{Date.today}.csv", type: "text/csv"}
+      format.csv do
+      	headers['Content-Disposition'] = "attachment;filename=businesses.csv"
+      end
     end
   end
 
@@ -50,5 +51,10 @@ class BusinessesController < ApplicationController
     end
     def set_types
       @types = ["accounting","airport","amusement_park","aquarium","art_gallery","atm","bakery","bank","bar","beauty_salon","bicycle_store","book_store","bowling_alley","bus_station","cafe","campground","car_dealer","car_rental","car_repair","car_wash","casino","cemetery","church","city_hall","clothing_store","convenience_store","courthouse","dentist","department_store","doctor","electrician","electronics_store","embassy","establishment","finance","fire_station","florist","food","funeral_home","furniture_store","gas_station","general_contractor","grocery_or_supermarket","gym","hair_care","hardware_store","health","hindu_temple","home_goods_store","hospital","insurance_agency","jewelry_store","laundry","lawyer","library","liquor_store","local_government_office","locksmith","lodging","meal_delivery","meal_takeaway","mosque","movie_rental","movie_theater","moving_company","museum","night_club","painter","park","parking","pet_store","pharmacy","physiotherapist","place_of_worship","plumber","police","post_office","real_estate_agency","restaurant","roofing_contractor","rv_park","school","shoe_store","shopping_mall","spa","stadium","storage","store","subway_station","synagogue","taxi_stand","train_station","transit_station","travel_agency","university","veterinary_care","zoo"]
+    end
+    def gen_csv(places)
+      places.each do |place|
+        businesses << [place.name, place.formatted_address, place.formatted_phone_number, place.types.map(&:humanize).join('/')]
+      end.to_csv
     end
 end
